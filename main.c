6773d497ee3c1df8c2b91d867103ccdd32a45158
dudelim <137728498+dudelim@users.noreply.github.com>
2023-07-06 15:44:49 +0000

    1

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  char frase[50];
+  int i, tamanho;
+
+  printf("Input: ");
+  fgets(frase, 50, stdin);
+
+  
 }
